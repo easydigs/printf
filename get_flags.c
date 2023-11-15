949aa325285ac03cf984e034fc5d5661292f42dc
nbaali <144456478+nbaali@users.noreply.github.com>
2023-11-15 01:08:12 +0100

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -3,34 +3,36 @@
 /**
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
- * @i: Pointer to the current position in the format string
- * Return: Flags
+ * @i: take a parameter.
+ * Return: Flags:
  */
 int get_flags(const char *format, int *i)
 {
-    int j, curr_i;
-    int flags = 0;
-    const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-    const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int j, curr_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-    // Loop through the characters in the format string after the current position
-    for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-    {
-        // Check if the current character is one of the flag characters
-        for (j = 0; FLAGS_CH[j] != '\0'; j++)
-        {
-            if (format[curr_i] == FLAGS_CH[j])
-            {
-                // Set the corresponding flag in the flags variable
-                flags |= FLAGS_ARR[j];
-                break;
-            }
-        }
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
 
-        // If the character is not one of the flag characters, break out of the loop
-        if (FLAGS_CH[j] == '\0')
-            break;
-    }
+		if (FLAGS_CH[j] == 0)
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (flags);
+}
+
 
     // Update the current position in the format string
     *i = curr_i - 1;
